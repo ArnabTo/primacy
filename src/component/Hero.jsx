53f10143f47bd41b3072664484150a380cecf498
ArnabTo <arnabs929@gmail.com>
2024-08-26 21:58:26 +0600

    updated  hero

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,20 +1,16 @@
-import { Button } from 'antd';
-import HeroBg from '../assets/hero.jpg'
 import HeroBg2 from '../assets/hero2.jpg'
-import HeroBg3 from '../assets/hero3.jpg'
+
 const Hero = () => {
     return (
-        <div className="relative w-full h-screen bg-cover bg-no-repeat" style={{ backgroundImage: `url(${HeroBg2})` }}>
-            {/* Background Overlay */}
+        <div className="relative w-full h-[40rem] lg:h-screen bg-cover bg-no-repeat" style={{ backgroundImage: `url(${HeroBg2})` }}>
             <div className="absolute inset-0 bg-black/50"></div>
 
-            {/* Content of the Hero Section */}P
             <div className="relative z-10 flex flex-col justify-center items-center h-full text-white">
                 <div className='flex flex-col justify-center items-center space-y-5'>
                     <h1 className="text-5xl font-extrabold text-center font-lato">Your New Beginning <span className='text-heroHeadline'>Starts Here!</span></h1>
                     <p className="mt-4 text-xl text-center font-lato">Discover how our studio can transform your approach to fitness <br></br> and well-being</p>
                     <button
-                        className="bg-white w-full lg:w-1/3 text-textColor px-4 py-3 rounded-md text-base font-semibold font-lato hover:bg-btnBg hover:text-white transition-all delay-75">
+                        className="bg-white lg:w-1/3 text-textColor px-4 py-3 rounded-md text-base font-semibold font-lato hover:bg-btnBg hover:text-white transition-all delay-75">
                             <a href='#'>Book Your Free Evaluation</a>
                     </button>
                 </div>
