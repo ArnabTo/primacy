d17e408ea2570b8a3f0dbc69bb9162a6b31b3ffd
ArnabTo <arnabs929@gmail.com>
2024-08-26 22:00:14 +0600

    updated navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -91,11 +91,10 @@ const Navbar = () => {
                             </div>
                             <div>
                                 <button onClick={toggleMenu} aria-label="Menu" className="menu-toggler text-black">
-                                    <svg width="26" height="20" viewBox="0 0 26 20" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M1 1H25" stroke="#00000" strokeWidth="1.5" strokeLinecap="round" />
-                                        <path d="M1 10H25" stroke="#00000" strokeWidth="1.5" strokeLinecap="round" />
-                                        <path d="M1 19H25" stroke="#00000" strokeWidth="1.5" strokeLinecap="round" />
+                                    <svg width="26" height="20" viewBox="0 0 26 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M1 1H25" stroke="#000" strokeWidth="1.5" strokeLinecap="round" />
+                                        <path d="M1 10H25" stroke="#000" strokeWidth="1.5" strokeLinecap="round" />
+                                        <path d="M1 19H25" stroke="#000" strokeWidth="1.5" strokeLinecap="round" />
                                     </svg>
                                 </button>
                                 <div
