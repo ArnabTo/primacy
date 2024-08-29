f6c15d925c25c0732c406c9a42bc17133c866745
ArnabTo <arnabs929@gmail.com>
2024-08-29 17:57:29 +0600

    updated about and service to porto

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -6,20 +6,21 @@ const About = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex flex-col lg:flex-row items-start justify-between  mx-3 lg:mx-20">
                     <div className="lg:w-1/2 mb-8 lg:mb-0 lg:pr-8">
-                        <h2 className="text-2xl text-aboutTxtColor uppercase mb-10">About</h2>
+                        <h2 className="text-2xl text-aboutTxtColor uppercase mb-10">Sobre Nós</h2>
                         <p className="text-lg text-gray-700 leading-relaxed tracking-wider font-normal">
                             <span className="block text-2xl lg:text-4xl font-bold uppercase">
-                                I am Carolina, with about 7 years of experience as a personal trainer.
+                                Eu sou a Carolina, tenho cerca de 7 anos de experiência como personal trainer.
                             </span>
-                            <br></br> 
-                            I am committed to providing specialized support that goes beyond physical training.
-                            My focus is to ensure that each client receives the guidance, motivation, and follow-up necessary to achieve their goals. 
-                            <br></br> 
-                            <br></br> 
-                            At Primacy, we believe that each person is unique and deserves a training plan that reflects their individual needs and objectives. We are a studio where personalized attention is our key differentiator.
+                            <br></br>
+                            Estou comprometida em oferecer um suporte especializado que vai além do treino físico.
+                            O meu foco é garantir que cada cliente receba a orientação, motivação e acompanhamento necessário para que cada um possa alcançar os seus objetivos.
+                            <br></br>
+                            <br></br>
+                            Na Primacy, acreditamos que cada pessoa é única e merece um plano de treino que reflita as suas necessidades e objetivos individuais.
+                            Somos um estúdio, onde a atenção personalizada é o nosso diferencial.
                         </p>
                     </div>
-                    <div className="w-">
+                    <div className="mx-auto">
                         <img src={AboutImg} alt="About Carolina" className="w-full h-auto object-cover rounded-lg shadow-lg" />
                     </div>
                 </div>
--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -7,7 +7,7 @@ const Services = () => {
         <div className="bg-white pt-20 lg:pt-24 pb-36">
             <div className='max-w-7xl mx-auto'>
                 <div className='mt-20 my-24'>
-                    <h1 className="text-textColor mt-20 text-center text-3xl lg:text-4xl font-bold font-manrope uppercase mb-3">Our <span className='text-serviceTxtColor'>Services</span></h1>
+                    <h1 className="text-textColor mt-20 text-center text-3xl lg:text-4xl font-bold font-manrope uppercase mb-3">Os nossos  <span className='text-serviceTxtColor'>serviços</span></h1>
                     <p className='text-textColor text-sm lg:text-xl text-center font-manrope mx-3'>Unlock your potential with our expert personal training, assessments, <br /> and nutritional guidance.</p>
                 </div>
 
@@ -20,7 +20,7 @@ const Services = () => {
                                     className="w-full h-full object-cover transform scale-110 transition-transform duration-[800ms] ease-[cubic-bezier(0.5,0,0.1,1)] group-hover:scale-100 brightness-100 group-hover:brightness-50"
                                 />
                                 <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 opacity-0 group-hover:opacity-100">
-                                    <p className="text-white text-lg lg:text-2xl font-bold uppercase">Personal Training</p>
+                                    <p className="text-white text-lg lg:text-2xl font-bold uppercase">Treino personalizado</p>
                                 </div>
                             </div>
                         </div>
@@ -33,7 +33,7 @@ const Services = () => {
                                     className="w-full h-full object-cover transform scale-110 transition-transform duration-[800ms] ease-[cubic-bezier(0.5,0,0.1,1)] group-hover:scale-100 brightness-100 group-hover:brightness-50"
                                 />
                                 <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 opacity-0 group-hover:opacity-100">
-                                    <p className="text-white text-lg lg:text-2xl font-bold uppercase">Physical Assessments</p>
+                                    <p className="text-white text-lg lg:text-2xl font-bold uppercase">Avaliações físicas</p>
                                 </div>
                             </div>
                         </div>
@@ -46,7 +46,7 @@ const Services = () => {
                                     className="w-full h-full object-cover transform scale-110 transition-transform duration-[800ms] ease-[cubic-bezier(0.5,0,0.1,1)] group-hover:scale-100 brightness-100 group-hover:brightness-50"
                                 />
                                 <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 opacity-0 group-hover:opacity-100">
-                                    <p className="text-white text-lg lg:text-2xl font-bold uppercase">Nutritional Guidance</p>
+                                    <p className="text-white text-lg lg:text-2xl font-bold uppercase">Acompanhamento nutricional</p>
                                 </div>
                             </div>
                         </div>
