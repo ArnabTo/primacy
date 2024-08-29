81462994e9a55a71be8fdfbfc615a1dc2350879a
ArnabTo <arnabs929@gmail.com>
2024-08-29 17:35:03 +0600

    updated service

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -6,43 +6,49 @@ const Services = () => {
     return (
         <div className="bg-white pt-20 lg:pt-24 pb-36">
             <div className='max-w-7xl mx-auto'>
-                <div className=' mt-20 my-24'>
+                <div className='mt-20 my-24'>
                     <h1 className="text-textColor mt-20 text-center text-3xl lg:text-4xl font-bold font-manrope uppercase mb-3">Our <span className='text-serviceTxtColor'>Services</span></h1>
-                    <p className='text-textColor text-sm lg:text-xl text-center font-manrope mx-3'>Unlock your potential with our expert personal training, assessments, <br></br> and nutritional guidance.</p>
+                    <p className='text-textColor text-sm lg:text-xl text-center font-manrope mx-3'>Unlock your potential with our expert personal training, assessments, <br /> and nutritional guidance.</p>
                 </div>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-5">
                     <a href='/personal-training'>
-                        <div className="group">
-                            <div className='overflow-hidden h-[25rem] '>
+                        <div className="group relative">
+                            <div className='overflow-hidden h-[16rem] lg:h-[20rem]'>
                                 <img
                                     src={Service1}
-                                    className="w-full h-full object-cover transform transition-transform duration-[800ms] ease-[cubic-bezier(0.5,0,0.1,1)] group-hover:scale-125"
+                                    className="w-full h-full object-cover transform scale-110 transition-transform duration-[800ms] ease-[cubic-bezier(0.5,0,0.1,1)] group-hover:scale-100 brightness-50 group-hover:brightness-100"
                                 />
+                                <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 opacity-0 group-hover:opacity-100">
+                                    <p className="text-white text-lg lg:text-2xl font-bold uppercase">Personal Training</p>
+                                </div>
                             </div>
-                            <p className="text-textColor text-lg lg:text-2xl text-start font-bold my-10 uppercase">Personal Training</p>
                         </div>
                     </a>
                     <a href='/physical-assessments'>
-                        <div className="group">
-                            <div className='overflow-hidden h-[25rem] '>
+                        <div className="group relative">
+                            <div className='overflow-hidden h-[16rem] lg:h-[20rem]'>
                                 <img
                                     src={Service2}
-                                    className="w-full h-full object-cover transform transition-transform duration-[800ms] ease-[cubic-bezier(0.5,0,0.1,1)] group-hover:scale-125"
+                                    className="w-full h-full object-cover transform scale-110 transition-transform duration-[800ms] ease-[cubic-bezier(0.5,0,0.1,1)] group-hover:scale-100 brightness-50 group-hover:brightness-100"
                                 />
+                                <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 opacity-0 group-hover:opacity-100">
+                                    <p className="text-white text-lg lg:text-2xl font-bold uppercase">Physical Assessments</p>
+                                </div>
                             </div>
-                            <p className="text-textColor text-lg lg:text-2xl text-start font-bold my-10 uppercase">Physical Assessments</p>
                         </div>
                     </a>
                     <a href='/nutritional-guidance'>
-                        <div className="group">
-                            <div className='overflow-hidden h-[25rem] '>
+                        <div className="group relative">
+                            <div className='overflow-hidden h-[16rem] lg:h-[20rem]'>
                                 <img
                                     src={Service3}
-                                    className="w-full h-full object-cover transform transition-transform duration-[800ms] ease-[cubic-bezier(0.5,0,0.1,1)] group-hover:scale-125"
+                                    className="w-full h-full object-cover transform scale-110 transition-transform duration-[800ms] ease-[cubic-bezier(0.5,0,0.1,1)] group-hover:scale-100 brightness-50 group-hover:brightness-100"
                                 />
+                                <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 opacity-0 group-hover:opacity-100">
+                                    <p className="text-white text-lg lg:text-2xl font-bold uppercase">Nutritional Guidance</p>
+                                </div>
                             </div>
-                            <p className="text-textColor text-lg lg:text-2xl text-start font-bold my-10 uppercase">Nutritional Guidance</p>
                         </div>
                     </a>
                 </div>
