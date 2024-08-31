d86f6ed14ca4d14744c0c06393f019f92188d3e2
ArnabTo <arnabs929@gmail.com>
2024-08-31 17:20:38 +0600

    updated service ui

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -14,7 +14,7 @@ const Services = () => {
                 <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-5 mx-10">
                     <a href='/personal-training'>
                         <div className="group relative">
-                            <div className='overflow-hidden w-[15rem] h-[15rem] lg:w-[20rem] lg:h-[20rem] mx-auto'> {/* Adjusted width and height */}
+                            <div className='overflow-hidden w-[15rem] h-[15rem] lg:w-[20rem] lg:h-[20rem] mx-auto  rounded-md'> 
                                 <img
                                     src={Service1}
                                     className="w-full h-full object-cover transform scale-110 transition-transform duration-[800ms] ease-[cubic-bezier(0.5,0,0.1,1)] group-hover:scale-100 brightness-100 group-hover:brightness-50"
@@ -27,7 +27,7 @@ const Services = () => {
                     </a>
                     <a href='/physical-assessments'>
                         <div className="group relative">
-                            <div className='overflow-hidden w-[15rem] h-[15rem] lg:w-[20rem] lg:h-[20rem] mx-auto'> {/* Adjusted width and height */}
+                            <div className='overflow-hidden w-[15rem] h-[15rem] lg:w-[20rem] lg:h-[20rem] mx-auto rounded-md'> 
                                 <img
                                     src={Service2}
                                     className="w-full h-full object-cover transform scale-110 transition-transform duration-[800ms] ease-[cubic-bezier(0.5,0,0.1,1)] group-hover:scale-100 brightness-100 group-hover:brightness-50"
@@ -40,7 +40,7 @@ const Services = () => {
                     </a>
                     <a href='/nutritional-guidance'>
                         <div className="group relative">
-                            <div className='overflow-hidden w-[15rem] h-[15rem] lg:w-[20rem] lg:h-[20rem] mx-auto'> {/* Adjusted width and height */}
+                            <div className='overflow-hidden w-[15rem] h-[15rem] lg:w-[20rem] lg:h-[20rem] mx-auto rounded-md'>
                                 <img
                                     src={Service3}
                                     className="w-full h-full object-cover transform scale-110 transition-transform duration-[800ms] ease-[cubic-bezier(0.5,0,0.1,1)] group-hover:scale-100 brightness-100 group-hover:brightness-50"
