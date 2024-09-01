5060bdff86f79c7f8527473be469fd48723e315f
ArnabTo <arnabs929@gmail.com>
2024-09-01 23:27:06 +0600

    updated service pics

--- src/component/sub-routes/NutritionalGuidance.jsx
+++ src/component/sub-routes/NutritionalGuidance.jsx
@@ -1,4 +1,6 @@
-
+import NgService1 from '../../assets/ng-service1.jpg'
+import NgService2 from '../../assets/ng-service2.jpg'
+import NgService3 from '../../assets/ng-service3.jpg'
 const NutritionalGuidance = () => {
     return (
         <section className="bg-white mb-44">
@@ -12,16 +14,16 @@ const NutritionalGuidance = () => {
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 h-full">
                         <div className="col-span-2 sm:col-span-1 md:col-span-2 bg-gray-50 h-auto md:h-full flex flex-col">
                             <span className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow">
-                                <img src="https://img.freepik.com/free-photo/medium-shot-doctor-showing-results-patient_23-2148302133.jpg?t=st=1725130497~exp=1725134097~hmac=9e5abf7cb3e5e1e851eca009c802fc97f4cd63252bf4eb0663b831751638e0fe&w=1380" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
+                                <img src={NgService1} alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
                             </span>
                         </div>
                         <div className="col-span-2 sm:col-span-1 md:col-span-2 bg-stone-50">
                             <span className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 mb-4">
-                                <img src="https://img.freepik.com/free-photo/healthy-natural-super-food_53876-123908.jpg?t=st=1725130559~exp=1725134159~hmac=170f94cc32762640b039e7c840ddba5d440daaa5a33763516ecbd08d68600f45&w=1380" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
+                                <img src={NgService2} alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
                             </span>
                             <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 lg:grid-cols-2">
                                 <span className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
-                                    <img src="https://img.freepik.com/free-photo/medium-shot-doctor-showing-results-patient_23-2148302133.jpg?w=1380&t=st=1724779753~exp=1724780353~hmac=c6c5ff051a64d655ea8dd009d7707a540707f8fe793884b7294a610403f45c07" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
+                                    <img src={NgService3} alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
                                 </span>
                                 <span className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
                                     <img src="https://img.freepik.com/free-photo/smiling-nutritionist-advises-young-patient-woman-proper-nutrition-dieting_496169-1469.jpg?w=996&t=st=1724772405~exp=1724773005~hmac=84a394ddfe6b27e3f98cceb43f307dc5eec48ffda7c5db08e37a4614b90bcfdb" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
--- src/component/sub-routes/PhysicalAssessments.jsx
+++ src/component/sub-routes/PhysicalAssessments.jsx
@@ -1,4 +1,6 @@
-
+import PaService1 from '../../assets/pa-service.jpg'
+import PaService2 from '../../assets/pa-service2.jpg'
+import PaService3 from '../../assets/pa-service3.jpg'
 
 const PhysicalAssessments = () => {
     return (
@@ -12,19 +14,19 @@ const PhysicalAssessments = () => {
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 h-full">
                         <div className="col-span-2 sm:col-span-1 md:col-span-2 bg-gray-50 h-auto md:h-full flex flex-col">
                             <span className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow">
-                                <img src="https://img.freepik.com/free-photo/doctor-helping-patient-rehabilitation_23-2150321566.jpg?t=st=1725130296~exp=1725133896~hmac=6998adac694bdab27defcdbb5e5a819def81b578cb522d1ea5b084d02cb1e8d8&w=1380" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
+                                <img src={PaService1} alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
                             </span>
                         </div>
                         <div className="col-span-2 sm:col-span-1 md:col-span-2 bg-stone-50">
                             <span className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 mb-4">
-                                <img src="https://img.freepik.com/free-photo/doctor-helping-patient-rehabilitation_23-2150321624.jpg?t=st=1725130289~exp=1725133889~hmac=545e95beac1bf0827cbf126be2084f25392980fc4e1c3a758fe200d7234cadfc&w=1380" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
+                                <img src={PaService2} alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
                             </span>
                             <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 lg:grid-cols-2">
                                 <span className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
                                     <img src="https://plus.unsplash.com/premium_photo-1661901743106-cfc613fdf5bb?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
                                 </span>
                                 <span className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40">
-                                    <img src="https://img.freepik.com/free-photo/doctor-helping-patient-rehabilitation_23-2150321559.jpg?t=st=1725130382~exp=1725133982~hmac=4fec9604ebce11d560debfb1d88e5d8560c968109f27e720b1d899e68e11cbf7&w=1380" alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
+                                    <img src={PaService3} alt="" className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" />
                                 </span>
                             </div>
                         </div>
